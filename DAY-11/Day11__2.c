5781fdca6c8e95af1bfa2fec911b863ed48521e5
nilay-amd <nilay11412@gmail.com>
2026-06-11 19:08:00 +0530

    DAY-11 uploaded

---
+++ DAY-11/Day11__2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int max(int a, int b){
+    return (a>b)?a:b;
+}
+
+int main(){
+    int x, y, result;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    result = max(x, y);
+
+    printf("Maximum = %d", result);
+
+    return 0;
+}
